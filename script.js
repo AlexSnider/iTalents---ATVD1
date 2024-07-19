beff70814963d914ee06eea59bf7cf8d6e34a442
AlexSnider <alexsnider8@gmail.com>
2024-07-19 15:30:21 -0300

    refactor: chages "calc" var for "imc", refacts output

--- script.js
+++ script.js
@@ -4,25 +4,26 @@
 // IMC = peso / (altura * altura)
 // classificação: abaixo do peso, peso normal, sobrepeso e obesidade.
 function calcularIMC(peso, altura) {
-  const calc = peso / (altura * altura); // constante (calc) para o calculo do IMC.
+  const imc = peso / (altura * altura); // constante (imc) para o calculo do IMC.
   let classificacao; // variável (classificacao) para a classificação do IMC. let sendo usado dentro do bloco de código.
 
-  if (calc < 18.5) { // condicinais para a classificação do IMC. 
+  if (imc < 18.5) {
+    // condicinais para a classificação do IMC.
     // classificação abaixo do peso.
     classificacao = "abaixo do peso";
-  } else if (calc >= 18.5 && calc <= 24.9) {
+  } else if (imc >= 18.5 && imc <= 24.9) {
     // classificação peso normal.
     classificacao = "peso normal";
-  } else if (calc >= 25 && calc <= 29.9) {
+  } else if (imc >= 25 && imc <= 29.9) {
     // classificação sobrepeso.
     classificacao = "sobrepeso";
-  } else if (calc >= 30) {
+  } else if (imc >= 30) {
     // classificação obesidade.
     classificacao = "obesidade";
   }
   return {
     // retorna um objeto contendo o IMC e a sua classificação.
-    calc: calc.toFixed(2), // arredonda o calculo do IMC para 2 casas decimais.
+    imc: imc.toFixed(2), // arredonda o calculo do IMC para 2 casas decimais.
     classificacao: classificacao, // retorna a classificação do IMC.
   };
 }
@@ -39,8 +40,12 @@ exemplos.forEach((exemplo) => {
   // percorre cada objeto (exemplo) do array.
   const resultado = calcularIMC(exemplo.peso, exemplo.altura); // constante (resultado) para o calculo do IMC e a sua classificação.
   console.log(
-    `Peso: ${exemplo.peso}kg, Altura: ${exemplo.altura}m -> IMC: ${resultado.calc}, Classificação: ${resultado.classificacao}`
+    `Peso: ${exemplo.peso}kg, Altura: ${exemplo.altura}m -> IMC: ${resultado.imc}, Classificação: ${resultado.classificacao}`
   ); // imprime o calculo do IMC e a sua classificação usando `template literals`.
 });
 
-console.log(calcularIMC(60, 1.75)); // imprime o calculo do IMC e a sua classificação.
+const resultado = calcularIMC(85, 1.8); // constante (resultado) para o calculo do IMC e a sua classificação.
+
+console.log(
+  `\nO cálculo do IMC para os valores referidos são: IMC: ${resultado.imc}, Classificação: ${resultado.classificacao}.`
+); // imprime o calculo do IMC e a sua classificação.
